b9d09487454291f2662fb638291c6e9fd2ac9dc5
beibei <beibeijiaxiong@gmail.com>
2026-03-01 15:30:26 +0800

    axios基础配置

--- rabbit/src/main.js
+++ rabbit/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// 测试接口
+import { getCategory } from '@/apis/testAPI'
+getCategory().then(res => {
+  console.log(res)
+})
+
 createApp(App).mount('#app')
